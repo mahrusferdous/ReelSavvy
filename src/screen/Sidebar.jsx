d6e0701799ce6ee976962d9d3e8fa972c5d674aa
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-06-30 20:54:43 +0600

    chore: Remove unused Clock.svg and update icon-home.svg stroke color

--- src/screen/Sidebar.jsx
+++ src/screen/Sidebar.jsx
@@ -2,6 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import styles from "../styles/Sidebar.module.css";
 import logoLoader from "../assets/logos/icon-loader.svg";
+import logoHome from "../assets/logos/icon-home.svg";
+import logoDiscovery from "../assets/logos/icon-frame.svg";
+import logoCommunity from "../assets/logos/icon-group.svg";
+import logoRecent from "../assets/logos/icon-clock.svg";
+import logoCollection from "../assets/logos/icon-star.svg";
+import logoSettings from "../assets/logos/icon-settings.svg";
+import logoHelp from "../assets/logos/icon-question.svg";
 
 const Sidebar = () => {
     return (
@@ -10,33 +17,33 @@ const Sidebar = () => {
             <p className={styles.pTag}>MAIN</p>
             <nav className="nav flex-column mx-5">
                 <Link to="/" className={styles.navLink}>
-                    <i class="bi bi-house-door"></i> Home
+                    <img src={logoHome} alt="home" /> Home
                 </Link>
                 <Link to="/" className={styles.navLink}>
-                    <i class="bi bi-film"></i> Discovery
+                    <img src={logoDiscovery} alt="discovery" /> Discovery
                 </Link>
                 <Link to="/" className={styles.navLink}>
                     <img src={logoLoader} alt="loader" /> Mood Feature
                 </Link>
                 <Link to="/" className={styles.navLink}>
-                    Community
+                    <img src={logoCommunity} alt="community" /> Community
                 </Link>
             </nav>
             <p className={styles.pTag}>LIBRARY</p>
             <nav className="nav flex-column mx-5 mb-5">
                 <Link to="/" className={styles.navLink}>
-                    Recent
+                    <img src={logoRecent} alt="recent   " /> Recent
                 </Link>
                 <Link to="/" className={styles.navLink}>
-                    My Collection
+                    <img src={logoCollection} alt="collection" /> Collection
                 </Link>
             </nav>
             <nav className="nav flex-column mx-5">
                 <Link to="/" className={styles.navLink}>
-                    Settings
+                    <img src={logoSettings} alt="settings" /> Settings
                 </Link>
                 <Link to="/" className={styles.navLink}>
-                    Help
+                    <img src={logoHelp} alt="help" /> Help
                 </Link>
             </nav>
         </div>
