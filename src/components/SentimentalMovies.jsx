ff877b4834931bebe33e564858c3aa7eb50c77b2
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-07-02 08:42:37 -0400

    feat: Add lazy loading for movie posters in NostalgicMovies and SentimentalMovies components

--- src/components/SentimentalMovies.jsx
+++ src/components/SentimentalMovies.jsx
@@ -1,10 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { fetchSentimentalMovies } from "../api/tmdbApi";
 import styles from "../styles/PopularMovies.module.css";
+import { IdContext } from "../context/IdContext";
+import { useNavigate } from "react-router-dom";
 
 const SentimentalMovies = () => {
     const [movies, setMovies] = useState([]);
     const posterURL = "https://image.tmdb.org/t/p/original";
+    const navigate = useNavigate();
+    const { setId } = useContext(IdContext);
 
     useEffect(() => {
         fetchSentimentalMovies()
@@ -21,7 +25,14 @@ const SentimentalMovies = () => {
             <h1>Sentimental Movies</h1>
             <div>
                 {movies.map((movie) => (
-                    <div key={movie.id} className={styles.poster}>
+                    <div
+                        key={movie.id}
+                        onClick={() => {
+                            setId(movie.id);
+                            navigate("/movie");
+                        }}
+                        className={styles.poster}
+                    >
                         <img src={`${posterURL}${movie.poster_path}`} alt={movie.title} />
                     </div>
                 ))}
