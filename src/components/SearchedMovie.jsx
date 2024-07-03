7310fffab81d60e934a067e015692724c3ef7407
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-07-03 14:41:35 -0400

    refactor: Update SearchedMovie component to use CSS module for styling

--- src/components/SearchedMovie.jsx
+++ src/components/SearchedMovie.jsx
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { IdContext } from "../context/IdContext";
+import styles from "../styles/SearchMovie.module.css";
 
 const SearchedMovie = ({ movie }) => {
     const posterURL = "https://image.tmdb.org/t/p/w500";
@@ -12,15 +13,9 @@ const SearchedMovie = ({ movie }) => {
 
     return (
         <div>
-            <div>
+            <div className={styles.content}>
                 <Link to={"/movie"} onClick={() => movieProcess()}>
-                    {/* <h2>{movie.title}</h2> */}
-                    {/* <h3>Release Date: {movie.release_date}</h3>
-                <h3>Popularity: {movie.popularity}</h3>
-                <h3>Ratings: {movie.vote_average}</h3> */}
                     <img src={`${posterURL}${movie.poster_path}`} alt={movie.title} />
-                    {/* <img src={`${posterURL}${movie.backdrop_path}`} alt={movie.title} />
-                <p>{movie.overview}</p> */}
                 </Link>
             </div>
         </div>
