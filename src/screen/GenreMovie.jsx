c4382ed01cdacb9c915beb726316544675ba81cd
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-07-05 04:25:16 -0400

    refactor: Lazy load movie posters in GenreMovie and Sidebar components

--- src/screen/GenreMovie.jsx
+++ src/screen/GenreMovie.jsx
@@ -10,6 +10,28 @@ const GenreMovie = ({ genreId }) => {
     const [genreMovies, setGenreMovies] = useState([]);
     const { setId } = useContext(IdContext);
 
+    const genres = [
+        { id: 28, name: "Action" },
+        { id: 12, name: "Adventure" }, //
+        { id: 16, name: "Animation" },
+        { id: 35, name: "Comedy" },
+        { id: 80, name: "Crime" },
+        { id: 99, name: "Documentary" },
+        { id: 18, name: "Drama" },
+        { id: 10751, name: "Family" },
+        { id: 14, name: "Fantasy" },
+        { id: 36, name: "History" },
+        { id: 27, name: "Horror" },
+        { id: 10402, name: "Music" },
+        { id: 9648, name: "Mystery" },
+        { id: 10749, name: "Romance" },
+        { id: 878, name: "Science Fiction" },
+        { id: 10770, name: "TV Movie" },
+        { id: 53, name: "Thriller" },
+        { id: 10752, name: "War" },
+        { id: 37, name: "Western" }, //
+    ];
+
     useEffect(() => {
         fetchGenreMovies(genreId)
             .then((data) => {
@@ -24,8 +46,13 @@ const GenreMovie = ({ genreId }) => {
         return <h1>Loading...</h1>;
     }
 
+    const genreName = genres.find((genre) => genre.id === genreId).name;
+
     return (
         <div className={styles.main_content}>
+            <h2 style={{ color: "white" }} key={genreId}>
+                {genreName}
+            </h2>
             <div>
                 {genreMovies.map((movie) => (
                     <div key={movie.id} className={styles.poster}>
