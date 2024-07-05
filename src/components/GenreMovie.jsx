f8c8f23a6906d6fc4d2a9b97ad6c12901d2261d4
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-07-05 08:50:12 -0400

    refactor: Lazy load movie posters in GenreMovie and Sidebar components

--- src/components/GenreMovie.jsx
+++ src/components/GenreMovie.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { fetchGenreMovies } from "../api/tmdbApi";
 import { Link } from "react-router-dom";
-import Sidebar from "./Sidebar";
+import Sidebar from "../screen/Sidebar";
 import styles from "../styles/GenreMovie.module.css";
 import { IdContext } from "../context/IdContext";
 
--- src/screen/GenrePage.jsx
+++ src/screen/GenrePage.jsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { IdsContext } from "../context/IdsContext";
-import GenreMovie from "./GenreMovie";
+import GenreMovie from "../components/GenreMovie";
 import Sidebar from "./Sidebar";
 import { MoodContext } from "../context/MoodContext";
 import styles from "../styles/GenrePage.module.css";
