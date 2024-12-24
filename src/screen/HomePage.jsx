0faa8dbeeb6a55c3420d46d306eb94778c9071af
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-12-24 18:30:06 -0500

    refactor: Clean up formatting and improve CSS styles for HomePage, Footer, and Sidebar components

--- src/screen/HomePage.jsx
+++ src/screen/HomePage.jsx
@@ -13,83 +13,83 @@ import NostalgicMovies from "../components/NostalgicMovies";
 import Footer from "./Footer";
 
 const HomePage = () => {
-    const [movies, setMovies] = useState([]);
-    const posterURL = "https://image.tmdb.org/t/p/original";
-    const { setId } = useContext(IdContext);
-    const navigation = useNavigate();
+	const [movies, setMovies] = useState([]);
+	const posterURL = "https://image.tmdb.org/t/p/original";
+	const { setId } = useContext(IdContext);
+	const navigation = useNavigate();
 
-    useEffect(() => {
-        fetchNowPlaying()
-            .then((data) => {
-                const shuffleMovies = shuffleArray(data);
-                setMovies(shuffleMovies.slice(0, 5));
-            })
-            .catch((error) => {
-                console.error("Error fetching movies:", error);
-            });
-    }, [fetchNowPlaying]);
+	useEffect(() => {
+		fetchNowPlaying()
+			.then((data) => {
+				const shuffleMovies = shuffleArray(data);
+				setMovies(shuffleMovies.slice(0, 5));
+			})
+			.catch((error) => {
+				console.error("Error fetching movies:", error);
+			});
+	}, [fetchNowPlaying]);
 
-    const shuffleArray = (array) => {
-        const shuffled = array.slice(); // Create a copy of the array
-        for (let i = shuffled.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
-        }
-        return shuffled;
-    };
+	const shuffleArray = (array) => {
+		const shuffled = array.slice(); // Create a copy of the array
+		for (let i = shuffled.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+		}
+		return shuffled;
+	};
 
-    const handleAddToWatchList = (movie) => {
-        const storedWatchList = JSON.parse(localStorage.getItem("watchList")) || [];
-        const updatedWatchList = [...storedWatchList, movie];
-        localStorage.setItem("watchList", JSON.stringify(updatedWatchList));
-    };
+	const handleAddToWatchList = (movie) => {
+		const storedWatchList = JSON.parse(localStorage.getItem("watchList")) || [];
+		const updatedWatchList = [...storedWatchList, movie];
+		localStorage.setItem("watchList", JSON.stringify(updatedWatchList));
+	};
 
-    return (
-        <div style={{ backgroundColor: "#050505" }}>
-            <Sidebar />
-            <div className={styles.main_content}>
-                <Carousel>
-                    {movies.map(
-                        (movie) =>
-                            movie.backdrop_path && (
-                                <Carousel.Item
-                                    key={movie.id}
-                                    onClick={() => {
-                                        setId(movie.id);
-                                        navigation("/movie");
-                                    }}
-                                >
-                                    <img
-                                        className="d-block w-100"
-                                        src={`${posterURL}${movie.backdrop_path}`}
-                                        alt={movie.title}
-                                    />
-                                    <Carousel.Caption>
-                                        <p className={styles.captionP}>TOP RECOMMENDED MOVIES</p>
-                                        <h1>{movie.title}</h1>
-                                        <button className={styles.heart} onClick={() => handleAddToWatchList(movie)}>
-                                            <img src={iconHeart} alt="heart" />
-                                        </button>
-                                    </Carousel.Caption>
-                                </Carousel.Item>
-                            )
-                    )}
-                </Carousel>
-            </div>
+	return (
+		<div style={{ backgroundColor: "#050505" }}>
+			<Sidebar />
+			<div className={styles.main_content}>
+				<Carousel>
+					{movies.map(
+						(movie) =>
+							movie.backdrop_path && (
+								<Carousel.Item
+									key={movie.id}
+									onClick={() => {
+										setId(movie.id);
+										navigation("/movie");
+									}}
+								>
+									<img
+										className="d-block w-100"
+										src={`${posterURL}${movie.backdrop_path}`}
+										alt={movie.title}
+									/>
+									<Carousel.Caption>
+										<p className={styles.captionP}>TOP RECOMMENDED MOVIES</p>
+										<h1>{movie.title}</h1>
+										<button className={styles.heart} onClick={() => handleAddToWatchList(movie)}>
+											<img src={iconHeart} alt="heart" />
+										</button>
+									</Carousel.Caption>
+								</Carousel.Item>
+							)
+					)}
+				</Carousel>
+			</div>
 
-            <PopularMovies />
-            <SentimentalMovies />
-            <NostalgicMovies />
+			<PopularMovies />
+			<SentimentalMovies />
+			<NostalgicMovies />
 
-            <div className={styles.banner}>
-                <img onClick={() => navigation("/mood")} className="img-fluid" src={bannerLogo} alt="Banner" />
-            </div>
+			<div className={styles.banner}>
+				<img onClick={() => navigation("/mood")} className="img-fluid" src={bannerLogo} alt="Banner" />
+			</div>
 
-            <div style={{ marginLeft: "377px" }}>
-                <Footer />
-            </div>
-        </div>
-    );
+			<div className={styles.footerSytle}>
+				<Footer />
+			</div>
+		</div>
+	);
 };
 
 export default HomePage;
