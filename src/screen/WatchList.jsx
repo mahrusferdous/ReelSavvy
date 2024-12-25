2292dccbc8d4d74d8c5bf8c9a8aabc4121d30915
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-12-24 19:02:02 -0500

    refactor: Update WatchList component layout and styles for improved responsiveness

--- src/screen/WatchList.jsx
+++ src/screen/WatchList.jsx
@@ -7,63 +7,62 @@ import { useNavigate } from "react-router-dom";
 import Footer from "./Footer";
 
 const WatchList = () => {
-    const [watchList, setWatchList] = useState([]);
-    const { setId } = useContext(IdContext);
-    const navigation = useNavigate();
+	const [watchList, setWatchList] = useState([]);
+	const { setId } = useContext(IdContext);
+	const navigation = useNavigate();
 
-    useEffect(() => {
-        const storedWatchList = JSON.parse(localStorage.getItem("watchList")) || [];
-        setWatchList(storedWatchList);
-    }, []);
+	useEffect(() => {
+		const storedWatchList = JSON.parse(localStorage.getItem("watchList")) || [];
+		setWatchList(storedWatchList);
+	}, []);
 
-    const handleRemoveItem = (id) => {
-        const updatedWatchList = watchList.filter((movie) => movie.id !== id);
-        localStorage.setItem("watchList", JSON.stringify(updatedWatchList));
-        setWatchList(updatedWatchList);
-    };
+	const handleRemoveItem = (id) => {
+		const updatedWatchList = watchList.filter((movie) => movie.id !== id);
+		localStorage.setItem("watchList", JSON.stringify(updatedWatchList));
+		setWatchList(updatedWatchList);
+	};
 
-    return (
-        <div>
-            <Sidebar />
+	return (
+		<div>
+			<Sidebar />
 
-            <div style={{ backgroundColor: "#050505" }}>
-                <h1>Watch List</h1>
-                <div className={styles.main_content}>
-                    {watchList &&
-                        watchList.map((movie) => (
-                            <div className={styles.content}>
-                                <div className={styles.movie_card}>
-                                    <img
-                                        onClick={() => {
-                                            setId(movie.id);
-                                            navigation("/movie");
-                                        }}
-                                        src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
-                                        alt={movie.title}
-                                    />
-                                    <button
-                                        style={{
-                                            backgroundColor: "transparent",
-                                            border: "none",
-                                            color: "white",
-                                            fontSize: "15px",
-                                            fontWeight: "bold",
-                                        }}
-                                        onClick={() => handleRemoveItem(movie.id)}
-                                    >
-                                        Remove
-                                        <img src={iconHeart} alt="heart" style={{ width: "30px" }} />
-                                    </button>
-                                </div>
-                            </div>
-                        ))}
-                </div>
-                <div style={{ marginLeft: "377px" }}>
-                    <Footer />
-                </div>
-            </div>
-        </div>
-    );
+			<div style={{ backgroundColor: "#050505" }}>
+				<div className={styles.main_content}>
+					{watchList &&
+						watchList.map((movie) => (
+							<div className={styles.content}>
+								<div className={styles.movie_card}>
+									<img
+										onClick={() => {
+											setId(movie.id);
+											navigation("/movie");
+										}}
+										src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
+										alt={movie.title}
+									/>
+									<button
+										style={{
+											backgroundColor: "transparent",
+											border: "none",
+											color: "white",
+											fontSize: "15px",
+											fontWeight: "bold",
+										}}
+										onClick={() => handleRemoveItem(movie.id)}
+									>
+										Remove
+										<img src={iconHeart} alt="heart" style={{ width: "30px" }} />
+									</button>
+								</div>
+							</div>
+						))}
+				</div>
+				<div className={styles.footer_left}>
+					<Footer />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default WatchList;
