37b35be7859a90ee0cbbe3fc6d05891794306598
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-12-24 19:40:11 -0500

    refactor: Update MovieTrailer component styles for improved responsiveness

--- src/components/MovieTrailer.jsx
+++ src/components/MovieTrailer.jsx
@@ -3,50 +3,49 @@ import { fetchMovieTrailer } from "../api/tmdbApi";
 import styles from "../styles/MovieInfo.module.css";
 
 const MovieTrailer = ({ movieId }) => {
-    const [trailers, setTrailers] = useState(null);
-    const [trailer, setTrailer] = useState(null);
-    const [text, setText] = useState("Loading...");
+	const [trailers, setTrailers] = useState(null);
+	const [trailer, setTrailer] = useState(null);
+	const [text, setText] = useState("Loading...");
 
-    useEffect(() => {
-        // Fetch the movie trailers when movieId changes
-        fetchMovieTrailer(movieId)
-            .then((data) => {
-                setTrailers(data);
-            })
-            .catch((error) => {
-                console.error("Error fetching movie trailer:", error);
-            });
-    }, [movieId]); // Only movieId is necessary
+	useEffect(() => {
+		// Fetch the movie trailers when movieId changes
+		fetchMovieTrailer(movieId)
+			.then((data) => {
+				setTrailers(data);
+			})
+			.catch((error) => {
+				console.error("Error fetching movie trailer:", error);
+			});
+	}, [movieId]); // Only movieId is necessary
 
-    useEffect(() => {
-        // Find and set the trailer when trailers change
-        if (trailers) {
-            const trailer = trailers.find((trailer) => trailer.type === "Trailer");
-            setTrailer(trailer);
-        }
-    }, [trailers]);
+	useEffect(() => {
+		// Find and set the trailer when trailers change
+		if (trailers) {
+			const trailer = trailers.find((trailer) => trailer.type === "Trailer");
+			setTrailer(trailer);
+		}
+	}, [trailers]);
 
-    setTimeout(() => {
-        setText("Not Available");
-    }, 5000);
+	setTimeout(() => {
+		setText("Not Available");
+	}, 5000);
 
-    return (
-        <div className={styles.trailer}>
-            {trailer ? (
-                <div key={trailer.id}>
-                    <iframe
-                        width="800"
-                        height="450"
-                        src={`https://www.youtube.com/embed/${trailer.key}`}
-                        title={trailer.name}
-                        allowFullScreen
-                    ></iframe>
-                </div>
-            ) : (
-                <p style={{ color: "white" }}>{text}</p> // Loading state or message
-            )}
-        </div>
-    );
+	return (
+		<div className={styles.trailer}>
+			{trailer ? (
+				<div key={trailer.id}>
+					<iframe
+						className={styles.trailer_frame}
+						src={`https://www.youtube.com/embed/${trailer.key}`}
+						title={trailer.name}
+						allowFullScreen
+					></iframe>
+				</div>
+			) : (
+				<p style={{ color: "white" }}>{text}</p> // Loading state or message
+			)}
+		</div>
+	);
 };
 
 export default MovieTrailer;
